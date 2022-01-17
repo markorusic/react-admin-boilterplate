85474c206a4d0a7e2c409f201706afd383d6c750
Marko Rusic <marko.rusic@novafabrika.rs>
2022-01-17 06:25:53 +0100

    rm clg

--- src/core/http-client.ts
+++ src/core/http-client.ts
@@ -3,5 +3,3 @@ import axios from 'axios'
 export let http = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL?.toString() ?? ''
 })
-
-console.log(import.meta.env.VITE_API_BASE_URL?.toString() ?? '')
